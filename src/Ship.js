383c6fc56c973ca649cdc9d76305ebb08cda818d
Anastasiya <avojtesik1@gmail.com>
2022-01-09 12:31:57 +0300

    add class Ship and methods in additional

--- src/Ship.js
+++ src/Ship.js
@@ -1,1 +1,11 @@
-class Ship{}
+class Ship{
+    size=null;
+    direction=null;
+    killed=false;
+
+
+    constructor(size, direction){
+        this.size=size;
+        this.direction=direction;
+    }
+}
