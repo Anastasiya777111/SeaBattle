28163f615485a1bc44218e58b795564c645136ab
Anastasiya <avojtesik1@gmail.com>
2022-01-09 13:03:06 +0300

    add class FieldView

--- src/FieldView.js
+++ src/FieldView.js
@@ -1,1 +1,22 @@
-class FieldView extends Field {}
+class FieldView extends Field {
+    root=null;
+table=null;
+dock=null;
+polygon=null;
+
+constructor(){
+    const root=document.createElement('div');
+    root.classList.add('fieldOfBattle');
+
+    const table=document.createElement('table');
+    table.classList.add('fieldOfBattle-table');
+
+    const dock=document.createElement('div');
+    dock.classList.add('fieldOfBattle-dock');
+
+    const polygon=document.createElement('div');
+    polygon.classList.add('fieldOfBattle-polygon');
+
+    Object.assign(this, {root, table, dock, polygon});
+}
+}
